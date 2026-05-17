2dfd10735240f101a6c015d3af4d6468b3b33354
mu <772927148@qq.com>
2026-05-17 09:43:28 +0800

    ohos: split harmony clipboard bridge

---
+++ harmony/app/entry/src/main/cpp/harmony_clipboard_bridge.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "freerdp_runtime.h"
+
+#include <functional>
+#include <memory>
+#include <string>
+
+#if defined(HARMONY_HAS_FREERDP_HEADERS)
+#include <freerdp/freerdp.h>
+#endif
+
+namespace rdp_bridge {
+
+#if defined(HARMONY_HAS_FREERDP_HEADERS)
+class HarmonyClipboardBridge {
+public:
+    using LogFn = std::function<void(const std::string&)>;
+
+    HarmonyClipboardBridge();
+    ~HarmonyClipboardBridge();
+
+    bool Initialize(rdpContext* context, FreerdpRuntimeApi& api, const LogFn& log, std::string& error);
+    void Uninitialize();
+
+private:
+    struct Impl;
+    std::unique_ptr<Impl> impl_;
+};
+#endif
+
+} // namespace rdp_bridge
