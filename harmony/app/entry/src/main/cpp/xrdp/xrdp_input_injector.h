e75a3bdebd617381a8ff45249e19344768ef24c1
mu <772927148@qq.com>
2026-05-21 15:37:20 +0800

    xrdp: consume ohos backend ABI

--- harmony/app/entry/src/main/cpp/xrdp/xrdp_input_injector.h
+++ harmony/app/entry/src/main/cpp/xrdp/xrdp_input_injector.h
@@ -3,20 +3,11 @@
 #include <cstdint>
 #include <string>
 
+#include "ohos/xrdp_ohos.h"
+
 namespace rdp_bridge {
 
-struct XrdpOhosInputEvent {
-    int32_t version = 0;
-    int32_t msg = 0;
-    long param1 = 0;
-    long param2 = 0;
-    long param3 = 0;
-    long param4 = 0;
-    int32_t width = 0;
-    int32_t height = 0;
-    int32_t bpp = 0;
-    int32_t connected = 0;
-};
+using XrdpOhosInputEvent = ::xrdp_ohos_input_event;
 
 bool DispatchXrdpInputEvent(const XrdpOhosInputEvent& event, std::string& message);
 void PrimeXrdpInputInjectorAuthorization(const std::string& reason);
