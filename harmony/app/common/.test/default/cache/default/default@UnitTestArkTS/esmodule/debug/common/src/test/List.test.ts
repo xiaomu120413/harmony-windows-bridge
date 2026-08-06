c18ab0a67f630f1ff783985fe4f620baecd569e3
mu <772927148@qq.com>
2026-08-06 17:29:31 +0800

    chore(ohos): remove generated common outputs

--- harmony/app/common/.test/default/cache/default/default@UnitTestArkTS/esmodule/debug/common/src/test/List.test.ts
+++
@@ -1,8 +0,0 @@
-import deviceCapabilityPolicyTest from "@normalized:N&&&common/src/test/DeviceCapabilityPolicy.test&";
-import rdpConnectionValidatorTest from "@normalized:N&&&common/src/test/RdpConnectionValidator.test&";
-import windowLayoutPolicyTest from "@normalized:N&&&common/src/test/WindowLayoutPolicy.test&";
-export default function testsuite(): void {
-    deviceCapabilityPolicyTest();
-    rdpConnectionValidatorTest();
-    windowLayoutPolicyTest();
-}
